6b0349455351205b3b67947a0561cad41fd22214
Arieleon25 <Arieleon25@yahoo.com>
2023-03-15 20:56:35 -0400

    Add project files.

---
+++ MethodSub/OptionMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub
+{
+    public class OptionMath
+    {
+        public void Om1 (int i, int i2 = 0)
+        {
+            int num1 = i + 5;
+            int numOp = i2 - 2;
+            Console.WriteLine(num1);
+            Console.WriteLine("{0}", numOp);
+
+        }
+    }
+}
---
+++ MethodSub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionMath OptionNum = new OptionMath();
+            Console.WriteLine("Input 2 numbers, one at a time. Second number is optional: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            OptionNum.Om1(userInput, userInput2);
+
+            Console.ReadLine();
+        }
+    }
+}
